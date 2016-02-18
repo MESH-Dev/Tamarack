78e4a77cf607037d78575d4210264afcad9463b4
robbiesh <sherrard.rob@gmail.com>
2016-02-18 18:23:46 -0500

    creative network

---
+++ single-directorylisting.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
+  <div class="row">
+
+    <?php get_template_part( 'partials/content', 'blocks' ); ?>
+
+  </div>
+
+  <div class="container">
+
+      <div class="twelve columns">
+
+          <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+              <h2><?php the_title(); ?></h2>
+
+              <?php the_content(); ?>
+
+          <?php endwhile; ?>
+
+      </div>
+
+  </div>
+
+</main><!-- #main -->
+
+<?php get_footer(); ?>
