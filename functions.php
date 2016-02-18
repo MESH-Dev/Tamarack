c52bb3c7e8e7b4fb65ce776bb10b9325caf993ed
robbiesh <sherrard.rob@gmail.com>
2016-02-18 16:20:03 -0500

    login

--- functions.php
+++ functions.php
@@ -105,7 +105,7 @@ add_action( 'login_form', 'username_or_email_login' );
 $s = site_url() . "/creative-network-profile";
 
 function acme_login_redirect( $redirect_to, $request, $user  ) {
-	return ( is_array( $user->roles ) && in_array( 'administrator', $user->roles ) ) ? admin_url() : $s;
+	return ( is_array( $user->roles ) && in_array( 'administrator', $user->roles ) ) ? admin_url() : site_url();
 }
 add_filter( 'login_redirect', 'acme_login_redirect', 10, 3 );
 
