59a5f0051b84912ca4c840e3ff891e61226b7c17
robbiesh <sherrard.rob@gmail.com>
2016-02-28 15:19:44 -0500

    wv profile page

---
+++ page-made-in-west-virginia-profile.php
@@ -0,0 +1,30 @@
+<?php
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
+  <div class="row">
+
+    <?php get_template_part('partials/account', 'header'); ?>
+
+  </div>
+
+  <div class="container">
+
+      <div class="eight columns offset-by-two">
+
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+
+        <?php endwhile; ?>
+        <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 9, 'title' => false, 'description' => false ) ); ?>
+
+      </div>
+
+  </div>
+
+</main><!-- #main -->
+
+<?php get_footer(); ?>
