cd133c81a465bce702951d631d97c804ceb43379
robbiesh <sherrard.rob@gmail.com>
2016-03-02 18:55:29 -0500

    remove title on text template

--- templates/text.php
+++ templates/text.php
@@ -11,8 +11,6 @@ get_header(); ?>
           <div class="content-full">
               <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-                  <h2><?php the_title(); ?></h2>
-
                   <div class="content <?php if(get_field('text_two_columns')) { ?> text-two-columns <?php } ?>">
                       <?php the_content(); ?>
                   </div>
