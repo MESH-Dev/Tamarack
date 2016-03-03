4c7b35931136d08d9a9820fe58b5e1a906139fbd
robbiesh <sherrard.rob@gmail.com>
2016-03-03 10:11:39 -0500

    single sidebar

--- single.php
+++ single.php
@@ -1,40 +1,46 @@
 <?php get_header(); ?>
 
+<main id="content">
 <div class="container">
-	<div class="nine columns offset-by-one">
-		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<div class="row">
+		<div class="nine columns offset-by-one">
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-			<div class="post">
-				<h2><?php the_title(); ?></h2>
-				<p class="postinfo"><?php the_author(); ?> | <?php the_time('F j, Y'); ?></p>
+				<div class="post">
+					<h2><?php the_title(); ?></h2>
+					<p class="postinfo"><?php the_author(); ?> | <?php the_time('F j, Y'); ?></p>
 
-				<hr>
+					<hr>
 
-				<div class="share-post">
-					<div class="share-icon">
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-facebook"></i> Share</a>
+					<div class="share-post">
+						<div class="share-icon">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-facebook"></i> Share</a>
+						</div>
+						<div class="share-icon">
+							<a href="https://twitter.com/home?status=<?php echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-twitter"></i> Tweet</a>
+						</div>
+						<!-- <div class="share-icon">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php // echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-pinterest"></i> Share</a>
+						</div>
+						<div class="share-icon">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php // echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-fancy"></i> Share</a>
+						</div> -->
+
 					</div>
-					<div class="share-icon">
-						<a href="https://twitter.com/home?status=<?php echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-twitter"></i> Tweet</a>
-					</div>
-					<!-- <div class="share-icon">
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php // echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-pinterest"></i> Share</a>
-					</div>
-					<div class="share-icon">
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php // echo str_replace(":", "%3A", get_permalink()); ?>"><i class="fa fa-fancy"></i> Share</a>
-					</div> -->
 
+					<?php the_content(); ?>
 				</div>
 
-				<?php the_content(); ?>
-			</div>
+				<?php // comments_template( '', true ); ?>
 
-			<?php // comments_template( '', true ); ?>
-
-		<?php endwhile; ?>
+			<?php endwhile; ?>
+		</div>
 	</div>
 </div>
 
+
 <?php get_sidebar(); ?>
+
+</main>
 
 <?php get_footer(); ?>
