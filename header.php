e0b386514bb812d1241216d804ef41f8d1e2a287
robbiesh <sherrard.rob@gmail.com>
2016-03-03 17:29:34 -0500

    single title

--- header.php
+++ header.php
@@ -341,6 +341,8 @@
 									echo "Oops!";
 								} elseif(get_post_type() == 'trail') {
 									echo get_the_title();
+								} elseif(is_single()) {
+									echo get_the_title();
 								}
 								else {
 									echo get_field("masthead_title");
